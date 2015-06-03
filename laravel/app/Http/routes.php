0b003146b581f9ec7ed0d40b70a2e253ef55446d
patrick <chhen.patrick@gmail.com>
2015-06-03 08:47:21 +0200

    Routes
    Les routes

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -19,3 +19,47 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+/*
+ * Route pour la gestion de la connexion
+ */
+Route::group(['middleware' => 'auth'], function() {
+    /*
+     * Controller d'interaction avec les comptes
+     */
+    Route::resource('utilisateur','UtilisateurController');
+    Route::resource('personne', 'PersonneController');
+    Route::resource('profile','ProfileController');
+    
+    /*
+     * ACL
+     */
+    Route::resource('role', 'RoleController');
+    Route::resource('permission', 'PermissionController');
+    /*
+     * Gestion du contenu de la G-R
+     */
+    Route::resource('gardeRobe', 'GardeRobeController');
+    /*
+     * Externe à FC
+     */
+    Route::resource('partenaire', 'PartenaireController');
+    /*
+     * Shooting des mannequins
+     */
+    Route::resource('shooting', 'ShootingController');
+
+});
+    /*
+     * Gestion des mannequins (pour affichage différent si pas co
+     */
+    Route::resource('mannequin', 'MannequinController');
+    /*
+     * Home page, affichage différent si pas co
+     */
+    Route::resource('homePage', 'homePageController');
+        /*
+     * Gestion session de votes
+     */
+    Route::resource('sessionVote', 'SessionVoteController');
+
